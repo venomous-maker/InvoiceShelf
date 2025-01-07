a88786e516afe62f3be689b01d6b42f0c621b9ff
venomous-maker <morganokumu8+1@gmail.com>
2025-01-07 18:52:54 +0300

    Added check migration command implementation

--- app/Console/Commands/CheckAndMigrate.php
+++ app/Console/Commands/CheckAndMigrate.php
@@ -3,6 +3,9 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\File;
+use Symfony\Component\Console\Command\Command as CommandAlias;
 
 class CheckAndMigrate extends Command
 {
@@ -22,9 +25,29 @@ class CheckAndMigrate extends Command
 
     /**
      * Execute the console command.
+     *
+     * @return int
      */
-    public function handle()
+    public function handle(): int
     {
-        //
+        $databasePath = config('database.connections.sqlite.database');
+
+        if (!File::exists($databasePath) || $databasePath === ':memory:') {
+            $this->error("SQLite database file not found at {$databasePath}");
+
+            if ($this->confirm('Do you want to create the SQLite database file?')) {
+                File::put($databasePath, '');
+
+                $this->info("SQLite database file created at {$databasePath}");
+            } else {
+                $this->info('Operation aborted. No database file was created.');
+                return CommandAlias::FAILURE;
+            }
+        }
+
+        $this->info('Running migrations...');
+        Artisan::call('migrate', [], $this->output);
+
+        return CommandAlias::SUCCESS;
     }
 }
