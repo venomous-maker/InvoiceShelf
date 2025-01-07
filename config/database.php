74d7a68f5c035d4d9c4d2974c5071aade02d144a
venomous-maker <morganokumu8+1@gmail.com>
2025-01-07 18:55:29 +0300

    Added sqlite config options

--- config/database.php
+++ config/database.php
@@ -32,6 +32,17 @@ return [
             'schema' => 'public',
             'sslmode' => 'prefer',
         ],
+        'sqlite' => [
+            'driver' => 'sqlite',
+            'url' => env('DB_URL'),
+            'database' => env('DB_DATABASE', database_path('database.sqlite')),
+            'prefix' => '',
+            'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
+            'busy_timeout' => null,
+            'journal_mode' => null,
+            'synchronous' => null,
+        ],
+
     ],
 
     'migrations' => [
